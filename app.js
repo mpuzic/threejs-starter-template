db50ec3aa38814f47945e92253888c993dc37ed8
Milan Pužić <milanpuzic@gmail.com>
2021-08-06 21:35:03 +0200

    Remove unnecessary comments.

--- app.js
+++ app.js
@@ -48,7 +48,6 @@ export default class Sketch {
 
     addObjects() {
         this.geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2)
-        // this.material = new THREE.MeshNormalMaterial({ side: THREE.DoubleSide })
         this.material = new THREE.ShaderMaterial({
             fragmentShader: fragment,
             vertexShader: vertex,
@@ -67,25 +66,6 @@ export default class Sketch {
         this.scene.add(this.mesh)
     }
 
-    // render() {
-    //     this.time += 1
-    //     this.mesh.rotation.x = this.time / 2000
-    //     this.mesh.rotation.y = this.time / 1000
-    //     this.renderer.render( this.scene, this.camera )
-    //     this.renderer.setAnimationLoop( this.render.bind(this) )
-    //     // Alternatively, we can use:
-    //     // window.requestAnimationFrame(this.render.bind(this))
-    // }
-
-    // When using arrow function syntax, we don't need to call `render.bind(this)`.
-    // The `.bind()` method binds the `this` to the calling class.
-    // ES6 made `this` in arrow functions reference the class rather than 
-    // the calling function.
-    // Arrow functions in class field properties seem useful because they’re autobind, 
-    // in short, no need to add `this.handleClick = this.handleClick.bind(this)` in the constructor
-    // More on this link:
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions#call_apply_and_bind
-
     render = () => {
         if (!this.isPlaying)
             return
@@ -100,8 +80,6 @@ export default class Sketch {
 
         this.renderer.render(this.scene, this.camera)
         this.renderer.setAnimationLoop(this.render)
-        // Alternatively, we can use:
-        // window.requestAnimationFrame(this.render)
     }
 
     guiControls() {
